e31b97e4c4a989aa26975e791792e1952c14d2c5
Zukhra <zuhra.galieva.2014@gmail.com>
2024-01-22 18:29:20 +0000

    Add index and template files

--- src/server.js
+++ src/server.js
@@ -1,3 +1,32 @@
 const express = require('express')
 const server = express()
-module.exports = server
+const { home } = require("./templates.js");
+
+const posts = [];
+
+server.get("/", (req, res) => {
+  const body = home(posts);
+  res.send(body);
+});
+
+server.post("/", express.urlencoded({ extended: false }), (req, res) => {
+  const nickname = req.body.nickname;
+  const message = req.body.message;
+  const errors = {};
+  if (!nickname) {
+    errors.nickname = "Please enter your nickname";
+  }
+  if (!message) {
+    errors.message = "Please enter a message";
+  }
+  if (Object.keys(errors).length) {
+    const body = home(posts, errors, req.body);
+    res.status(400).send(body);
+  } else {
+    const created = Date.now();
+    posts.push({ nickname, message, created });
+    res.redirect("/");
+  }
+});
+
+module.exports = server;
